59a79da70c17541f3d7d6a4f3c304da301f10942
rpet064 <93119631+rpet064@users.noreply.github.com>
2023-02-03 12:28:45 +1300

    customizing object placement for smaller screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,11 +14,13 @@ module.exports = {
       "black": "#000000",
       "white": "#ffffff",
       },
-      extend: {
-        backgroundImage: () => ({
-          meal: "url('./assets/images/1.jpg')",
-        }),
-    },
+      screens: {
+        'sm': {'min': '640px', 'max': '767px'},
+        'md': {'min': '768px', 'max': '1023px'},
+        'lg': {'min': '1024px', 'max': '1279px'},
+        'xl': {'min': '1280px', 'max': '1535px'},
+        '2xl': {'min': '1536px'},
+      },
     variants: {
       extend: {},
     },
